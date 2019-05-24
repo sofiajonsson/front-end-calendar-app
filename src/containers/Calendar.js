d1ad34dae33e0ecb3e397f7360fd0ec0ce3b5ca6
Sofia <jonssonsofia93@gmail.com>
2019-05-24 11:19:15 -0700

    Initial commit following creation

---
+++ src/containers/Calendar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Moment from 'moment';
+
+
+class Calendar extends React.Component {
+
+	render(){
+
+		return(
+					<h2>Calendar</h2>
+
+				);
+			}
+		}
+export default Calendar;
